ebe66ddc4dd4f79d423c1f4078f9e6019876f257
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2023-12-25 20:14:09 +0200

    Update app_functions.py

--- app/app_functions.py
+++ app/app_functions.py
@@ -4,11 +4,31 @@ import threading
 
 
 def update_progress_bar(progress, value, root):
+    """
+    Updates the progress bar with a new value.
+    Parameters:
+        progress (tkinter.ttk.Progressbar): The progress bar widget.
+        value (int): The new value for the progress bar.
+        root (tkinter.Tk): The root window.
+    Returns:
+        None
+    """
     progress['value'] = value
     root.update_idletasks()
 
 
 def extract_audio(progress, label_result, root):
+    """
+    Extracts audio from a video file and saves it as an MP3 file.
+
+    Args:
+        progress (int): The progress of the extraction process.
+        label_result (Label): The label to display the result of the extraction.
+        root (Tk): The root window of the application.
+
+    Returns:
+        None
+    """
     video_path = filedialog.askopenfilename()
     if video_path:
         audio_path = filedialog.asksaveasfilename(defaultextension=".mp3",
@@ -38,4 +58,11 @@ def extract_audio(progress, label_result, root):
 
 
 def start_extract_audio(progress, label_result, root):
+    """
+    Start the extraction of audio in a separate thread.
+
+    :param progress: A progress bar to track the extraction progress.
+    :param label_result: A label to display the result of the extraction.
+    :param root: The root directory where the extraction should take place.
+    """
     threading.Thread(target=extract_audio, args=(progress, label_result, root)).start()
